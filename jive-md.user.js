fcc923617205cec2f7c4fb6132411001e63022b8
Sam Hanes <sam.hanes@whitehatsec.com>
2015-06-11 14:05:44 -0700

    Add support for creation pages.

--- jive-md.user.js
+++ jive-md.user.js
@@ -2,11 +2,10 @@
 // @name        Jive Markdown
 // @namespace   maltera.com
 // @description Provide a Markdown editor in Jive.
-// @version     0.1
+// @version     0.2
 // @downloadURL https://raw.githubusercontent.com/Elemecca/jive-md/master/jive-md.user.js
-// @updateURL   https://raw.githubusercontent.com/Elemecca/jive-md/master/jive-md.user.js
 //
-// @include     /^https://[^.]+\.jiveon\.com/[^?#]+/edit([?#].*)?$/
+// @include     /^https://[^.]+\.jiveon\.com/([^?#]+/edit|[^?#/]+/(create|upload).jspa|blog/create-post.jspa)([?#].*)?$/
 //
 // @resource    md-html  https://raw.githubusercontent.com/neocotic/html.md/3.0.2/dist/md.min.js
 // @resource    md-conv  https://raw.githubusercontent.com/ujifgc/pagedown/f1ae0f5626/Markdown.Converter.js
@@ -51,22 +50,22 @@ loadScripts( function() {
   // scroll so the editor is reasonably positioned
   // Jive's enormous header is really annoying
   document.querySelector( '#jive-body header' ).scrollIntoView();
-    
+
   let mce = unsafeWindow.tinymce.EditorManager.get( 'wysiwygtext' );
   mce.save();
   mce.hide();
-  
+
   let output = document.getElementById( 'wysiwygtext' );
   output.style.display = 'none';
-  
+
   let wrapper = document.createElement( 'div' );
   wrapper.style.overflow = 'hidden';
-  
+
   let buttons = document.createElement( 'div' );
   buttons.id = 'wmd-button-bar';
   buttons.style.display = 'none';
   wrapper.appendChild( buttons );
-    
+
   let input = document.createElement( 'textarea' );
   input.id = 'wmd-input';
   input.style.float = 'left';
@@ -74,37 +73,37 @@ loadScripts( function() {
   input.style.height = '80vw';
   input.style.marginRight = '1em';
   wrapper.appendChild( input );
-  
+
   let preview = document.createElement( 'div' );
   preview.id = 'wmd-preview';
   preview.style.overflow = 'auto';
   wrapper.appendChild( preview );
-    
-  let panel = document.querySelector( '#edit-document .jive-editor-panel' );
+
+  let panel = document.querySelector( '#jive-body .jive-editor-panel' );
   panel.appendChild( wrapper );
-    
+
   try {
     input.value = Markdown.fromHTML( output.value );
   } catch (caught) {
     console.error( "error converting HTML", caught );
   }
-    
+
   let converter = Markdown.getSanitizingConverter();
   let editor = new Markdown.Editor( converter );
 
   let mceOpt = createObjectIn( unsafeWindow );
   mceOpt.format = 'raw';
-    
+
   converter.hooks.chain( 'postConversion', exportFunction( function (html) {
     mce.setContent( html, mceOpt );
     return html;
   }, unsafeWindow ) );
-  
+
   try {
     editor.run();
   } catch (caught) {
     console.error( "error starting PageDown", caught );
   }
-    
+
   input.focus();
 });
